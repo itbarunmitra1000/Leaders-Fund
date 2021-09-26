b21ffef1fd33221d38811dfeab54a99c27a117f9
Barun Mitra <itbarunmitra1000@gmail.com>
2021-09-26 22:17:28 +0600

    add header style and edit readme file

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Getting Started with Create React App
+# Make A Fund for a Charity Organizations
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            <h1>Top World Political Leaders Fund $ 10000000 </h1>
+        <div className= "header">
+            <h1>Make A Fund for a Charity Organizations </h1>
+            <h2>Top World Political Leaders Make a Fund for their Salarys</h2>
+            <h3>Total Need for Fund: $ 10000000</h3>
         </div>
     );
 };
